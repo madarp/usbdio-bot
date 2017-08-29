51ab1dd7f8aa511e5a396a4ff7ff2fd1f8beb9f8
Piero Madar <pmadar@tycoint.com>
2017-08-29 15:31:01 -0400

    Moved GPIO init into main.  Cleanup start & stop logging.

--- usbdio-bot/woody-bot.py
+++ usbdio-bot/woody-bot.py
@@ -5,6 +5,7 @@ WOODY-BOT
 A raspberry-pi slackbot that directly controls a 8-channel relay board via commands from a Slack channel.
 This uses the Slack RTM (Real Time Messaging) API.  The woody name derives from the fact that
 The raspi and the relay board are both mounted on a piece of scrap wood.
+This code is derived from https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
 
    Required environment variables (example only, these are not real tokens). Get these from the Slack account settings.
    BOT_ID = 'U20981S736'
@@ -32,11 +33,6 @@ BOT_COMMANDS = ['help', 'open', 'close', 'bounce', 'pulse', 'ping', 'exit']
 logger = logging.getLogger(__name__)
 sigterm_event = Event()
 bot_start = None
-
-# GPIO setup
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-GPIO_PINS = (17, 18, 27, 22, 23, 24, 25, 4)
 
 
 def sigterm_handler(sig_num, frame):
@@ -147,11 +143,12 @@ def parse_slack_output(slack_rtm_output):
 
 def post_message(slack_client, msg, chan=BOT_CHAN):
     """
-    Posts a message to a slack channel using the chat.PostMessage api
-    :param slack_client:
+    Posts a message to a slack channel using the chat.PostMessage api.
+    Swallows and logs any exceptions or slack_client errors.
+    :param slack_client: Instance of SlackClient to use for api call.
     :param msg: The text string message to post
     :param chan: The slack channel name to post in
-    :return:
+    :return: None
     """
     if slack_client is not None:
         try:
@@ -164,25 +161,28 @@ def post_message(slack_client, msg, chan=BOT_CHAN):
 if __name__ == "__main__":
 
     app_start_time = datetime.datetime.now()
-
-    print '\n'\
-        '-------------------------------------------------------------------\n'\
-        '    Running {0}\n'\
-        '    Started on {1}\n'\
-        '-------------------------------------------------------------------\n'\
-        .format(__file__, app_start_time.isoformat())
-
     logging.basicConfig(
         format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s [%(threadName)-12s] %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     logger.setLevel(logging.DEBUG)
 
+    logger.info(
+        '\n'
+        '-------------------------------------------------------------------\n'
+        '    Running {0}\n'
+        '    Started on {1}\n'
+        '-------------------------------------------------------------------\n'
+        .format(__file__, app_start_time.isoformat())
+    )
+
     # Install signal handlers after all startup services are running.
     signal.signal(signal.SIGINT, sigterm_handler)
     signal.signal(signal.SIGTERM, sigterm_handler)
 
     # Setup the GPIO relay control outputs
+    GPIO.setmode(GPIO.BCM)
+    GPIO_PINS = (17, 18, 27, 22, 23, 24, 25, 4)
     for p in GPIO_PINS:
         logger.info('{} Initializing GPIO.{}'.format(AT_BOT, p))
         GPIO.setup(p, GPIO.OUT)
@@ -229,14 +229,15 @@ if __name__ == "__main__":
             time.sleep(5.0)
             continue
 
-    GPIO.cleanup()
-    logging.shutdown()
-
     # Alas, we are dying
     uptime = datetime.datetime.now() - app_start_time
-    print '\n'\
-        '-------------------------------------------------------------------\n'\
-        '   Stopped {0}\n'\
-        '   Uptime was {1}\n'\
-        '-------------------------------------------------------------------\n'\
-        .format(__file__, str(uptime))
+    logger.info(
+        '\n'
+        '-------------------------------------------------------------------\n'
+        '   Stopped {0}\n'
+        '   Uptime was {1}\n'
+        '-------------------------------------------------------------------\n'
+        .format(__file__, str(uptime)))
+
+    GPIO.cleanup()
+    logging.shutdown()
